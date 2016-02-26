842283040226825405ad24c949fee262ab4e5e3c
fahadkhan2000 <fahad.khan@mapcase.de>
2016-02-26 15:56:48 +0100

    Completed first working version
    The code is working fine and passing the majority of common tests.

--- RegExpPolynomialSwift.playground/section-1.swift
+++ RegExpPolynomialSwift.playground/section-1.swift
@@ -1,63 +1,46 @@
-// Playground - noun: a place where people can play
-
 import UIKit
-
 import Foundation
-
-class regExpPol
-{
-    class func createTermsByRegExp(poly: String) -> [String]
-    {
+class regExpPol {
+    class func createTermsByRegExp(poly: String) -> [String] {
         var localPoly = poly.stringByReplacingOccurrencesOfString(" " , withString: "")
-        //var regexp2 = "([+-]?\\d*(?:\\.?\\d*))x(\\^(\\d*))?|([+-]\\d*(?:\\.?\\d*))"
         var regexp = "([+-]?\\d*(?:\\.?\\d+))?x(\\^(\\d*))?|([+-]?\\d*(?:\\.?\\d+))"
         var monomialsArray:[String] = []
         
-        if ((localPoly.rangeOfString(regexp, options: .RegularExpressionSearch)) != nil)
-        {
+        if ((localPoly.rangeOfString(regexp, options: .RegularExpressionSearch)) != nil) {
             println("matched..........")
-            monomialsArray = findAllStringsInRegexp(regexp, poly: poly)
-            
+            monomialsArray = findAllStringsInRegexp(regexp, poly: localPoly)
         }
-        println(monomialsArray)
         return (monomialsArray)
     }
     
-    class func findAllStringsInRegexp(regex: String, poly:String) -> [String]
-    {
+    class func findAllStringsInRegexp(regex: String, poly:String) -> [String] {
         var regex = NSRegularExpression(pattern: regex, options: nil, error: nil)
         var polyNSString = poly as NSString
         var matches = regex.matchesInString(poly, options: nil, range: NSMakeRange(0, polyNSString.length)) as [NSTextCheckingResult]
         return (map(matches) {polyNSString.substringWithRange($0.range)})
     }
     
-    class func determineTypeOfTermForSplit(singleTerm: String) -> [String]
-    {
+    class func determineTypeOfTermForSplit(singleTerm: String) -> [String] {
         var localString: String = singleTerm.stringByReplacingOccurrencesOfString("+", withString: "")
         var reformedSingleTerm: String
         
-        if(localString.rangeOfString("^") != nil)
-        {
+        if(localString.rangeOfString("^") != nil) {
             println("term with power\n")
             if(localString.hasPrefix("x") || localString.hasPrefix("-x") || localString.hasPrefix("+x"))
             {
                 reformedSingleTerm = localString.stringByReplacingOccurrencesOfString("x", withString: "1x")
             }
-            else
-            {
+            else {
                 reformedSingleTerm = localString.stringByReplacingOccurrencesOfString("x", withString: "x")
             }
             
         }
-        else
-        {
+        else {
             print("term without power\n")
-            if(localString.rangeOfString("x") != nil)
-            {
+            if(localString.rangeOfString("x") != nil) {
                 reformedSingleTerm = localString.stringByReplacingOccurrencesOfString("x", withString: "x^1")
             }
-            else
-            {
+            else {
                 println("constant term without power\n")
                 var appender = "x^0"
                 reformedSingleTerm = localString + appender
@@ -66,19 +49,16 @@ class regExpPol
         return (splitTermIntoCoeffAndPower(reformedSingleTerm))
     }
     
-    class func splitTermIntoCoeffAndPower(reformedSingleTerm: String) -> [String]
-    {
+    class func splitTermIntoCoeffAndPower(reformedSingleTerm: String) -> [String] {
         var splittedCoeffAndPower:[String]
         var localString = reformedSingleTerm
         
-        if (localString == "x^1" || localString == "-x^1" || localString == "+x^1")
-        {
+        if (localString == "x^1" || localString == "-x^1" || localString == "+x^1") {
             localString = localString.stringByReplacingOccurrencesOfString("x", withString: "1")
             splittedCoeffAndPower = split(localString) {$0 == "^"}
             print(splittedCoeffAndPower)
         }
-        else
-        {
+        else {
             localString = localString.stringByReplacingOccurrencesOfString("x", withString: "")
             splittedCoeffAndPower = split(localString) {$0 == "^"}
             print(splittedCoeffAndPower)
@@ -86,55 +66,37 @@ class regExpPol
         return splittedCoeffAndPower
     }
     
-    class func convertTermFromStringToDouble(splittedCoeffAndPower:[String]) -> (Double, Double)
-    {
+    class func convertTermFromStringToDouble(splittedCoeffAndPower:[String]) -> (Double, Double) {
         var localArray:[String] = splittedCoeffAndPower
         var coeffAndExpDoubleArray:[Double] = []
         
-        for(index, value) in enumerate(localArray)
-        {
+        for(index, value) in enumerate(localArray) {
             coeffAndExpDoubleArray.append((localArray[index] as NSString).doubleValue)
         }
-        println(coeffAndExpDoubleArray)
         return(coeffAndExpDoubleArray[0], coeffAndExpDoubleArray[1])
     }
     
-    class func evaluateTerm(singleTerm: String , val: Double) -> Double
-    {
+    class func evaluateTerm(singleTerm: String , val: Double) -> Double {
         var splittedCoeffAndPower = determineTypeOfTermForSplit(singleTerm)
         var (coeff, exp) = convertTermFromStringToDouble(splittedCoeffAndPower)
         return (coeff * (pow(val, exp)))
     }
     
-    class func calculateFinalResult(monomialsArray:[String] , val:Double) -> Double
-    {
+    class func calculateFinalResult(monomialsArray:[String] , val:Double) -> Double {
         var finalRes:Double = 0.0
         
-        for(index, value) in enumerate(monomialsArray)
-        {
+        for(index, value) in enumerate(monomialsArray) {
             finalRes = finalRes + evaluateTerm(value , val: val)
         }
         return finalRes
     }
     
-    class func run()
-    {
-        var poly = "3x^3 + 2x^2 + 1.1"
-        var val = 1.0
+    class func run() {
+        var poly = "4x^4 - 3x^3 + x - 10.01"
+        var val = 1.001
         var monomialsArray = createTermsByRegExp(poly)
         calculateFinalResult(monomialsArray, val: val)
     }
-
 }
-
-
-regExpPol.determineTypeOfTermForSplit("+1x^1")
-
-regExpPol.splitTermIntoCoeffAndPower("-3x^3")
-
-regExpPol.convertTermFromStringToDouble(["-1.1", "-1.1"])
-
-regExpPol.evaluateTerm("+2x^11", val: 2)
-
 regExpPol.run()
 
